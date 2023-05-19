df3d75b1b6d1d33cf87361e0d3efab263c20d836
Yuka Ishii <ishi0026@algonquinlive.com>
2023-05-19 12:43:51 -0400

    adds html, js files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//console.log("JavaScript is included!")
+/*
+the above list of code is commented out using a single line comment +
+/ or command + /
+this comment is a multiple comment, shortcut is shift + alt + A
+*/
+
