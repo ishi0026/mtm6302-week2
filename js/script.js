3b5039686acac566b7ac75dcde54cfc761ef00ed
Yuka Ishii <ishi0026@algonquinlive.com>
2023-05-19 13:47:09 -0400

    training

--- js/script.js
+++ js/script.js
@@ -2,6 +2,42 @@
 /*
 the above list of code is commented out using a single line comment +
 / or command + /
-this comment is a multiple comment, shortcut is shift + alt + A
+this comment is a multiple comment, shortcut is shift + alt + A or shift +option + A
 */
 
+
+let myName = "Yuka"
+console.log(myName)
+
+myName = "Yuka I"
+console.log(myName)//let can reassign value
+
+const address ="Earth"
+console.log(address)
+/*
+address = "Earth, Milkyway"
+console.log(address)//const can not reassign value
+ */
+let myProfession
+console.log(myProfession)
+
+myProfession = "Student"
+console.log(myProfession)
+
+const myScore = [52, 78, 90]
+console.log(myScore[1])
+
+//not confusing map and object
+//not confusing sets and arrays
+
+let num = 10
+num ++
+console.log(num)
+
+// let num = 10
+// num += 1
+// console.log(num) // ++ & += same thing
+
+console.log(num == "11")//== check data, === strictly check data type
+console.log(num > 90)
+console.log(num != 90)
